e6cdeea436b5bb3d9c13b35ed580c0f96173ce65
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2023-11-05 23:08:22 +0200

    Add build and test Action

--- Airport Ticket Booking/Paths.cs
+++ Airport Ticket Booking/Paths.cs
@@ -2,7 +2,7 @@
 {
     static public class Paths
     {
-        public static string Directory = @"..\..\..\..\..\DataBase\";
+        public static string Directory = @"..\..\..\..\DataBase\";
         public static string UserDBPath = Directory + "UserDB.csv";
         public static string FlightDBPath = Directory + "FlightDB.csv";
         public static string BookingDBPath = Directory + "BookingDB.csv";
