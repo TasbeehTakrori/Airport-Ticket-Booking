58ac5c952d21126eb787091a05fd7a79ab121884
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2023-11-06 11:29:03 +0200

    Add DB-Path env for yml and update path file

--- Airport Ticket Booking/Paths.cs
+++ Airport Ticket Booking/Paths.cs
@@ -2,9 +2,10 @@
 {
     static public class Paths
     {
-        public static string Directory = @"..\..\..\..\DataBase\";
-        public static string UserDBPath = Directory + "UserDB.csv";
-        public static string FlightDBPath = Directory + "FlightDB.csv";
-        public static string BookingDBPath = Directory + "BookingDB.csv";
+        public static readonly string databaseDirectory = Environment.GetEnvironmentVariable("DB_PATH");
+        public static readonly string UserDBPath = Path.Combine(databaseDirectory, "UserDB.csv");
+        public static readonly string FlightDBPath = Path.Combine(databaseDirectory, "FlightDB.csv");
+        public static readonly string BookingDBPath = Path.Combine(databaseDirectory, "BookingDB.csv");
+
     }
 }
